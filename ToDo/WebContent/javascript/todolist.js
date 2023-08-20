c2cb93d69a04c90ffa3d8aa27555f1b465b582df
Pranay A <pranay.a4795@gmail.com>
2023-08-20 23:26:00 +0100

    add skeleton files for ToDo app

---
+++ ToDo/WebContent/javascript/todolist.js
@@ -0,0 +1,16 @@
+/* Module for ToDo List application */
+/* Module for ToDo List application */
+var ToDoList = (function () {
+  /* add members here */
+
+  var init = function () {
+    /* add code to initialize this module */
+  };
+
+  /* execute the init function when the DOM is ready */
+  $(init);
+
+  return {
+    /* add members that will be exposed publicly */
+  };
+})();
