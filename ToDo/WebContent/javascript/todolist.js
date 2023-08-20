0d325e4f586431626ca8a031be91291eecdb7aee
Pranay A <pranay.a4795@gmail.com>
2023-08-21 00:17:59 +0100

    add tasks feature

--- ToDo/WebContent/javascript/todolist.js
+++ ToDo/WebContent/javascript/todolist.js
@@ -1,10 +1,31 @@
 /* Module for ToDo List application */
-/* Module for ToDo List application */
 var ToDoList = (function () {
   /* add members here */
+  var task = {
+    name: ko.observable(),
+    description: ko.observable(),
+  };
+
+  /* array of tasks */
+  var tasks = ko.observableArray();
+
+  var addTask = function () {
+    console.log("adding task");
+    tasks.unshift({
+      name: task.name(),
+      description: task.description(),
+    });
+    clearTask();
+  };
+
+  var clearTask = function () {
+    task.name(null);
+    task.description(null);
+  };
 
   var init = function () {
     /* add code to initialize this module */
+    ko.applyBindings(ToDoList);
   };
 
   /* execute the init function when the DOM is ready */
@@ -12,5 +33,8 @@ var ToDoList = (function () {
 
   return {
     /* add members that will be exposed publicly */
+    task: task,
+    tasks: tasks,
+    addTask: addTask,
   };
 })();
