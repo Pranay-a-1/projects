f7a0139a9cdea0e0c042ebee84681934faf21dde
Pranay A <pranay.a4795@gmail.com>
2023-08-28 22:06:53 +0100

    add skeleton project for customer registration form

---
+++ CustomerRegistrationCardForm/WebContent/javascript/registrationform.js
@@ -0,0 +1,16 @@
+/* Module for Registration form application */
+var RegistrationForm = (function () {
+  /* add members here */
+
+  var init = function () {
+    /* add code to initialize this module */
+    ko.applyBindings(RegistrationForm);
+  };
+
+  /* execute the init function when the DOM is ready */
+  $(init);
+
+  return {
+    /* add members that will be exposed publicly */
+  };
+})();
