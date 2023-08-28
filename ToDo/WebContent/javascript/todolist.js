3ace981839ae36c63393936251ae66b8b08d1fb5
Pranay A <pranay.a4795@gmail.com>
2023-08-28 22:00:47 +0100

    completed todo list project using knockoutjs

--- ToDo/WebContent/javascript/todolist.js
+++ ToDo/WebContent/javascript/todolist.js
@@ -4,7 +4,7 @@ var ToDoList = (function () {
   var task = {
     name: ko.observable(),
     description: ko.observable(),
-    status: ko.observable("new"),
+    priority: ko.observable(),
   };
 
   /* array of tasks */
@@ -15,6 +15,7 @@ var ToDoList = (function () {
     tasks.unshift({
       name: task.name(),
       description: task.description(),
+      priority: task.priority(),
       status: ko.observable(states.NEW),
     });
     clearTask();
@@ -23,6 +24,7 @@ var ToDoList = (function () {
   var clearTask = function () {
     task.name(null);
     task.description(null);
+    task.priority("1");
   };
 
   var deleteTask = function (task) {
@@ -38,9 +40,37 @@ var ToDoList = (function () {
     task.status(states.COMPLETE);
   };
 
+  /* observable to compute number of completed tasks */
+  var numOfCompletedTasks = ko.pureComputed(function () {
+    var completedTasks = ko.utils.arrayFilter(tasks(), function (task) {
+      return task.status() == states.COMPLETE;
+    });
+    return completedTasks.length;
+  });
+
   var states = {
     NEW: "new",
     COMPLETE: "complete",
+  };
+
+  /* method to sort the tasks by priority */
+  var sortByPriority = function () {
+    console.log("Sorting tasks by priority");
+    tasks.sort(function (left, right) {
+      return left.priority == right.priority
+        ? 0
+        : left.priority < right.priority
+        ? -1
+        : 1;
+    });
+  };
+
+  /* method to sort the tasks by name */
+  var sortByName = function () {
+    console.log("Sorting tasks by name");
+    tasks.sort(function (left, right) {
+      return left.name == right.name ? 0 : left.name < right.name ? -1 : 1;
+    });
   };
 
   var init = function () {
@@ -58,5 +88,8 @@ var ToDoList = (function () {
     addTask: addTask,
     deleteTask: deleteTask,
     completeTask: completeTask,
+    sortByPriority: sortByPriority,
+    sortByName: sortByName,
+    numOfCompletedTasks: numOfCompletedTasks,
   };
 })();
