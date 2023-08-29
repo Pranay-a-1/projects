7f6081ee1f52391a2ad9af28d70fe29cfad7f55e
Pranay A <pranay.a4795@gmail.com>
2023-08-29 16:49:21 +0100

    implemented form for customer registration form

--- CustomerRegistrationCardForm/WebContent/javascript/registrationform.js
+++ CustomerRegistrationCardForm/WebContent/javascript/registrationform.js
@@ -1,11 +1,58 @@
 /* Module for Registration form application */
 var RegistrationForm = (function () {
   /* add members here */
+  // model
+  var customer = {
+    personalInfo: {
+      firstName: ko.observable(),
+      middleName: ko.observable(),
+      lastName: ko.observable(),
+      title: ko.observable(),
+    },
+  };
 
   /* form submission */
   var submit = function () {
     console.log("The form is submitted");
+    console.log(ko.toJSON(customer));
   };
+
+  /* options for the title drop down*/
+  var titleOptions = [
+    {
+      value: "Mr",
+      setTitle: function () {
+        RegistrationForm.customer.personalInfo.title("Mr");
+      },
+    },
+    {
+      value: "Mrs",
+      setTitle: function () {
+        RegistrationForm.customer.personalInfo.title("Mrs");
+      },
+    },
+    {
+      value: "Miss",
+      setTitle: function () {
+        RegistrationForm.customer.personalInfo.title("Miss");
+      },
+    },
+    {
+      value: "Dr",
+      setTitle: function () {
+        RegistrationForm.customer.personalInfo.title("Dr");
+      },
+    },
+  ];
+
+  /* computed observable for title drop down text change */
+  var titleSelect = ko.pureComputed(function () {
+    if (customer.personalInfo.title() == null) {
+      return "select";
+    } else {
+      return customer.personalInfo.title();
+    }
+  });
 
   var init = function () {
     /* add code to initialize this module */
@@ -18,5 +65,8 @@ var RegistrationForm = (function () {
   return {
     /* add members that will be exposed publicly */
     submit: submit,
+    customer: customer,
+    titleOptions: titleOptions,
+    titleSelect: titleSelect,
   };
 })();
